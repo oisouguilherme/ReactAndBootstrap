da4d915cb1aa8965a9f09259af5b247f1f406fae
Guilherme Henrique <gh.dourado77@gmail.com>
2023-04-25 11:13:58 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import { Carousel } from './components/Carousel';
 import { Destaques } from './components/Destaques';
 import { Header } from './components/Header'
-import { MineBanner } from './components/MineBanner';
 
 function App() {
   return (
